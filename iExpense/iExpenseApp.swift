e3635a5d855144ee36aa17f14618463f7474949e
pegahtafviziingoampt <ingoampt@gmail.com>
2023-06-06 13:55:20 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Pegah Tafvizi on 06.06.23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
