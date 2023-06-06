6021268a5985a96f9071f704725476d22d476f76
pegahtafviziingoampt <ingoampt@gmail.com>
2023-06-06 14:30:05 +0200

    first commit

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "swift")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("new test!!")
         }
         .padding()
     }
